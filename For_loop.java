cdd1998696cb6c77c23fdfd4bfd18fea78da0775
vinodbargale <117828212+vinodbargale@users.noreply.github.com>
2023-06-05 14:45:30 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,53 @@
+package com_vinod_condition;
+
+public class For_loop 
+{
+public static void main(String[] args) 
+{
+	/*for(int x=10;x<=20;x++) {
+		System.out.println(x);
+	}
+	*/
+	/*for(int x=2;x<=100;x=x+2){
+		System.out.println(x);
+	}*/
+	int sum=0;
+	for(int x=1;x<=100;x++) {
+		sum=sum+x;
+	}
+		
+	System.out.println(sum);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}
+}
