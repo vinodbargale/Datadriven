d83587c620005233a4d3dd5b9b838f0bd4dfd9ab
vinodbargale <117828212+vinodbargale@users.noreply.github.com>
2023-06-05 14:23:15 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,30 @@
+package com_vinod_condition;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int a=15;
+		int b=18;
+		int c=19;
+		int big=0;
+		
+	if(a>b) {
+		
+		if(a>c)
+			big=a;
+		else big=c;
+	}
+	else {
+		if(b>c) 
+			big=b;
+			else big=c;
+		}
+	System.out.println(big);	
+		
+		
+		
+	}
+	}
+	
+
